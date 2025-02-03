4a5287bfeb91583a3ecc538b8be35288bf1a0055
avkiller <avkiller2012@gmail.com>
2025-02-03 22:22:14 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -91,8 +91,8 @@ const config = {
           artifactName: 'PicList-${version}-${arch}.${ext}'
         },
         win: {
+          "sign": false,
           icon: 'build/icons/icon.ico',
-          "signingOptions": [],
           // eslint-disable-next-line no-template-curly-in-string
           artifactName: 'PicList-Setup-${version}-${arch}.exe',
           verifyUpdateCodeSignature: false,
